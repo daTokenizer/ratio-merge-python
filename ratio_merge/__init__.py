3e547f5b032fe5036c5e394d841a2cfc077a4c48
Adam Lev-Libfeld <adam.inf@gmail.com>
2017-03-13 10:57:59 +0200

    add docstring

--- ratio_merge/__init__.py
+++ ratio_merge/__init__.py
@@ -1,12 +1,15 @@
 def ratio_merge(first_list, second_list, merge_ratio=0.5, desired_length=None, strict=False):
-    # This function acts as a generator producing elements from the two given lists by the given <merge_ratio>.
-    # The generator would preserve any internal order within the lists and would produce no more then 
-    # <desired_length> elements in total.
-    # In case the total number of elements withing either the lists is not enough to maintain the desired ratio
-    # throughout, the function would continue to generate elements from the remaining set until <desired_length> 
-    # elements were produced.
-    # If the strict parameter is set to True the function will stop producing elements once one of the lists has
-    # ran out of elemets. It whould also bias the produced list to fit closest to the length of the first list.
+    """ This function acts as a generator producing elements from the two given lists by the given <merge_ratio>.
+    The generator would preserve any internal order within the lists and would produce no more then <desired_length> 
+    elements in total.
+    
+    In case the total number of elements withing either the lists is not enough to maintain the desired ratio
+    throughout, the function would continue to generate elements from the remaining set until <desired_length> 
+    elements were produced.
+    
+    If the strict parameter is set to True the function will stop producing elements once one of the lists has
+    ran out of elemets. It whould also bias the produced list to fit closest to the length of the first list.
+    """
     current_ratio = 0.0
     current_length = 0
     first_list_pop_count = 0
