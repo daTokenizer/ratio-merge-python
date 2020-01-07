00ad5ec1866bbc8da27fa683f7f0992ce106db7b
daTokenizer <adam.inf@gmail.com>
2020-01-07 13:41:58 +0200

    advance to 0.3

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ with open("README.md", "r") as fh:
 setup(
   name = 'ratio_merge',
   packages = ['ratio_merge'],
-  version = '0.2',
+  version = '0.3',
   description = 'A small utility function for merging two lists by some ratio',
   long_description=long_description,
-  long_description_content_type="text/markdown",
   author = 'Adam Lev-Libfeld',
   author_email = 'adam@tamarlabs.com',
-  license='Apache 2.0'
+  license='Apache 2.0',
   url = 'https://github.com/daTokenizer/ratio-merge-python',
   download_url = 'https://github.com/daTokenizer/ratio-merge-python/archive/0.2.tar.gz',
   keywords = ['merge', 'ratio', 'lists'], # arbitrary keywords
